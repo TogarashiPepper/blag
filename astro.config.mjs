c16399751b88d57561a1ae8a480c9bdf7a8d5b80
Togarashi <togarashi@example.com>
2023-01-26 21:47:11 -0800

    commit mssage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://togarashipepper.github.io',
-    base: '/blag'
+  site: 'https://togarashipepper.github.io',
+  base: '/blag',
+  integrations: [tailwind()]
 });
