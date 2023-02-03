b14f092c45b7dccafa5273336bf8052d96af2c60
Togarashi <togarashi@example.com>
2023-02-02 23:07:20 -0800

    make mobile not cringe again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 import { defineConfig } from 'astro/config';
-import fs from "node:fs"
+import fs from "node:fs";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://togarashipepper.github.io',
   base: '/blag',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
-      theme: JSON.parse(fs.readFileSync('./src/shiki/one-dark-pro.json', 'utf-8')),
-    },
+      theme: JSON.parse(fs.readFileSync('./src/shiki/one-dark-pro.json', 'utf-8'))
+    }
   }
 });
